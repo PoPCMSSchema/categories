6baecef0a591647c3de58579999b7395b809afe9
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:42:45 +0800

    Added object type to $resultItem in `resolveValue`

--- src/FieldResolvers/CustomPostQueryableFieldResolver.php
+++ src/FieldResolvers/CustomPostQueryableFieldResolver.php
@@ -95,8 +95,22 @@ class CustomPostQueryableFieldResolver extends AbstractQueryableFieldResolver
         return parent::getFieldDefaultFilterDataloadingModule($typeResolver, $fieldName, $fieldArgs);
     }
 
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     * @return mixed
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ) {
         $categoryapi = \PoPSchema\Categories\FunctionAPIFactory::getInstance();
         $post = $resultItem;
         switch ($fieldName) {
